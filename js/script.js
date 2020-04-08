b4aad6fe9541ec9a10423dd99fb0b00fcfcc6960
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2020-04-08 18:01:19 -0400

    Added tag property and adjusted HTML links

--- js/script.js
+++ js/script.js
@@ -3,27 +3,28 @@ var quotes = [
 {
   quote: 'You only live as long as the last person who remembers you.',
   source: 'Akecheta',
-  citation: 'Westworld',
+  citation: 'Westworld'
 },
 {
   quote: `I'm not a follower. I'm a leader. And anyone who speaks their mind is always criticized.`,
   source: 'Tyler, the Creator',
-  year: 2013,
+  year: 2013
 },
 {
   quote: 'Live your life, live it right. Be different, do different things.',
-  source: 'Kendrick Lamar',
+  source: 'Kendrick Lamar'
 },
 {
   quote: 'I think that inside every adult is the heart of a child. \
   We just gradually convince ourselves that we have to act more like adults.',
   source: 'Shigeru Miyamoto',
+  tags: 'Gaming'
 },
 {
   quote: `We all live day to day completely oblivious to the fact \
   that we're a part of a much larger and stranger reality than we can \
   possibly imagine.` ,
-  source: 'Blake Crouch',
+  source: 'Blake Crouch'
 }
 ];
 
@@ -36,7 +37,7 @@ function getRandomQuote() {
 
 
 /*Creates a string of HTML to print the quote and source.
-If an object contains a citation or year property they will print as well.
+If an object contains a citation, tags, or year property they will print as well.
 If not, the object will print just the quote and source.*/
 function printQuote(){
   let randomQuote = getRandomQuote();
@@ -45,7 +46,9 @@ function printQuote(){
     html += `<span class="citation">${randomQuote.citation}</span></p>`;
   } else if (randomQuote.year){
     html += `<span class="year">${randomQuote.year}</span></p>`;
-  } else{
+  } else if (randomQuote.tags) {
+    html += `<span class="tags">${randomQuote.tags}</span></p>`;
+  }else{
     html += `</p>`;
   }
   //Allows the page to generate a new quote when the button is clicked.
