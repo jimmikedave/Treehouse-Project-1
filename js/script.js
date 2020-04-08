e2dd9e4acd2684981df18ab7eeb9a65404e5038e
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2020-04-08 09:06:20 -0400

    Added an array and tested

--- js/script.js
+++ js/script.js
@@ -3,15 +3,38 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
+// For assistance:
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
+/***
+ * `quotes` array
 ***/
-
-
+var array = [
+{
+  quote: 'test',
+  source: 'test',
+  citation: 'test',
+},
+{
+  quote: 'test',
+  source: 'test',
+  year: 'test',
+},
+{
+  quote: 'test',
+  source: 'test',
+},
+{
+  quote: 'test',
+  source: 'test',
+},
+{
+  quote: 'test',
+  source: 'test',
+}
+];
+console.log(array[0]);
 
 /***
  * `getRandomQuote` function
